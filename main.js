4063c72951ac3a1b599a9fbe6b29e8c6974c7202
Farhan-rashid10 <farkaf07@gmail.com>
2024-05-05 12:50:24 +0300

    group 7 work

---
+++ main.js
@@ -0,0 +1,88 @@
+let months = 1;
+let monthName;
+
+switch(months) {
+    case 1:
+        monthName = "January";
+    break;
+   
+    case 2 : 
+    monthName = "february";
+    
+    break;
+     
+    case 3 : 
+    monthName =  "march";
+    break;
+
+    case 4 : 
+    monthName =   "april";
+    break;
+
+    case 5 : 
+    monthName = "may";
+    break;
+
+    case 6 : 
+    monthName =  "june";
+    break;
+
+    case 7 : 
+    monthName = "july";
+    break;
+
+}
+    console.log("this month is " + monthName);
+
+
+
+let names = 2;
+let userName;
+
+switch(names) {
+    case 1:
+        userName = "samira";
+    break;
+   
+    case 2 : 
+       userName = "katra";
+    
+    break;
+     
+    case 3 : 
+       userName =  "farhan";
+    break;
+
+    case 4 : 
+      userName=   "sumeya";
+    break;
+
+    case 5 : 
+     userName = "amina";
+    break;
+
+    case 6 : 
+    userName= "yussuf";
+    break;
+
+    case 7 : 
+    userName = "abubakar";
+    break;
+
+}
+    console.log("my name is " + userName);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
